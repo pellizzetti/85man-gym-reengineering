45abda9dcc0866979c58d4cc9b4eb4f0618cfa25
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-04-09 20:46:14 -0300

    Remove useless `Promise.all`

--- src/database/migrations/20190409173846_create_persons.js
+++ src/database/migrations/20190409173846_create_persons.js
@@ -1,21 +1,19 @@
-exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTable('persons', (table) => {
-    table.increments('id').primary();
-    table.string('name').notNullable();
-    table.date('birthday');
-    table.string('gender', 1).defaultTo('F');
-    table.boolean('active').defaultTo(true);
-    table.string('phone');
-    table.string('cellphone');
-    table.string('email');
-    table.string('zipcode');
-    table.string('street');
-    table.string('number');
-    table.string('district');
-    table.string('city');
-    table.string('state');
-    table.timestamps(true, true);
-  }),
-]);
+exports.up = (knex, Promise) => knex.schema.createTable('persons', (table) => {
+  table.increments('id').primary();
+  table.string('name').notNullable();
+  table.date('birthday');
+  table.string('gender', 1).defaultTo('F');
+  table.boolean('active').defaultTo(true);
+  table.string('phone');
+  table.string('cellphone');
+  table.string('email');
+  table.string('zipcode');
+  table.string('street');
+  table.string('number');
+  table.string('district');
+  table.string('city');
+  table.string('state');
+  table.timestamps(true, true);
+});
 
-exports.down = (knex, Promise) => Promise.all([knex.schema.dropTable('persons')]);
+exports.down = (knex, Promise) => knex.schema.dropTable('persons');
